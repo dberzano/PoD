4946d1e28c37260eebf5c5a60f8fb89d3c47d2d6
Dario Berzano <dario.berzano@cern.ch>
2014-03-31 18:35:10 +0200

    Improved installation script

--- PoD_build.sh
+++ PoD_build.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 
-export PoDPrefix=$(cd `dirname "$0"`;pwd)
+#
+# PoD_build.sh -- by Dario Berzano <dario.berzano@cern.ch>
+#
+# Builds and installs PoD for SLC6. Directory structure expected:
+#
+# $PoDPrefix/
+#  - src/
+#     - build/                        # created by installer
+#     - PoD-<version>-Source.tar.gz   # you must download it
+#     - PoD-<version>-Source          # you must unpack it
+#
+# The source tarfile must be downloaded, unpacked and (maybe) patched manually.
+#
+# To install:
+#
+# $> cd <root_of_PoD_source>
+# $> ./PoD_build.sh --deep   # will run CMake + make install
+#
+# To recompile after edits:
+#
+# $> ./PoD_build.sh --shallow   # will make install only
+#
 
+# Check if we are running from the root of the source code
+cd $( dirname "$0" )
+if [ ! -e "BuildSetup.cmake" ] ; then
+  echo "The PoD build script is in the wrong directory"
+  exit 1
+fi
+
+# The PoD version
+if [[ $(basename "$PWD") =~ PoD-(.*)-Source ]] ; then
+  export PoDVersion="${BASH_REMATCH[1]}"
+  echo -e "\033[35mDetected PoD version: $PoDVersion\033[m"
+else
+  echo "Cannot detect PoD version from source directory name"
+  exit 1
+fi
+
+# The installation prefix
+export PoDPrefix=$(cd ../..;pwd)
+
+# Choose between deep and shallow installation: the first runs everything
+# including CMake after cleaning up the build directory, while the latter runs
+# only make install
 case "$1" in
   --deep) Deep=1 ;;
   --shallow) Deep=0 ;;
@@ -9,6 +52,8 @@ case "$1" in
 esac
 
 if [ $Deep == 1 ] ; then
+
+  # Clean up build and run CMake: requires interactive confirmation
 
   cd "$PoDPrefix"/src || exit 1
   rm -rf build
@@ -21,18 +66,18 @@ if [ $Deep == 1 ] ; then
     -DCMAKE_INSTALL_PREFIX="$PoDPrefix" \
     -DBoost_DEBUG="ON" || exit $?
 
-  read -p 'Is this OK? Type "Yes" to build and install: ' Ans
+  echo -n -e "\033[35mIs this OK? Type \"Yes\" to build and install: \033[m"
+  read Ans
   [ "$Ans" != 'Yes' ] && exit 1
 
 fi
 
+# Runs make install
 cd "$PoDPrefix"/src/build || exit 1
 make -j $(( $(grep -c bogomips /proc/cpuinfo) + 3 )) install || exit $?
 
-#source $PoDPrefix/PoD_env.sh || exit $?
-#pod-server getbins || exit $?
-
-# Create bins manually
+# Create worker node bins manually: assumes we are on SLC6 with libboost
+# installed "somewhere" and "accessible"
 cd "$PoDPrefix"/bin || exit 1
 rm -rf wn_bins
 mkdir -p wn_bins/pod-wrk-bin
@@ -46,9 +91,9 @@ cp -v \
   "$PoDPrefix"/lib/libSSHTunnel.so \
   .
 cd ..
-tar czvvf pod-wrk-bin-3.14-Linux-amd64.tar.gz pod-wrk-bin || exit $?
-touch pod-wrk-bin-3.14-Linux-x86.tar.gz
-touch pod-wrk-bin-3.14-Darwin-universal.tar.gz
+tar czvvf "pod-wrk-bin-${PoDVersion}-Linux-amd64.tar.gz" pod-wrk-bin || exit $?
+touch "pod-wrk-bin-${PoDVersion}-Linux-x86.tar.gz"
+touch "pod-wrk-bin-${PoDVersion}-Darwin-universal.tar.gz"
 rm -rf pod-wrk-bin
 
-echo All OK
+echo -e "\033[32mAll OK\033[m"
